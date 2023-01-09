e4520d9103bb5f1c238dee05882acbb8e11b932b
Avinash <avinash.bhat@mail.mcgill.ca>
2023-01-09 07:59:53 +0530

    Removed dark mode; defaulted to light mode

--- assets/js/toggle-light-dark.js
+++ assets/js/toggle-light-dark.js
@@ -1,29 +1,29 @@
-const btn = document.getElementById("toggle-dark-light");
+// const btn = document.getElementById("toggle-dark-light");
 
-let toggleIcon = btn.firstElementChild;
+// let toggleIcon = btn.firstElementChild;
 
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-btn.addEventListener("click", function () {
-    if (prefersDarkScheme.matches) {
-      	document.body.classList.toggle("light-theme");
-      	var theme = document.body.classList.contains("light-theme")
-        ? "light"
-        : "dark";
-      toggleIconFn(theme);
-    } else {
-    	document.body.classList.toggle("dark-theme");
-      	var theme = document.body.classList.contains("dark-theme")
-					? "dark"
-					: "light";
-      	toggleIconFn(theme);
-    }
-    localStorage.setItem("theme", theme);
-  });
+// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+// btn.addEventListener("click", function () {
+//     if (prefersDarkScheme.matches) {
+//       	document.body.classList.toggle("light-theme");
+//       	var theme = document.body.classList.contains("light-theme")
+//         ? "light"
+//         : "dark";
+//       toggleIconFn(theme);
+//     } else {
+//     	document.body.classList.toggle("dark-theme");
+//       	var theme = document.body.classList.contains("dark-theme")
+// 					? "dark"
+// 					: "light";
+//       	toggleIconFn(theme);
+//     }
+//     localStorage.setItem("theme", theme);
+//   });
 
-    const toggleIconFn = (theme) => {
-    if (theme === "dark") {
-      toggleIcon.className = "fa-solid fa-sun";
-    } else {
-      toggleIcon.className = "fa-solid fa-moon";
-    }
-  };
+//     const toggleIconFn = (theme) => {
+//     if (theme === "dark") {
+//       toggleIcon.className = "fa-solid fa-sun";
+//     } else {
+//       toggleIcon.className = "fa-solid fa-moon";
+//     }
+//   };
